7e2cdc8fd41f0f537d3764667f01b87f04ce9c8c
Jesse Daniels <info@jesseandgena.com>
2020-11-03 15:16:43 -0500

    added to exsisting commented project structure ideas to program file

--- Program.cs
+++ Program.cs
@@ -8,10 +8,11 @@ namespace MemoriesApp
         //ask if recording memory or quote
         //ask for date
         //prompt to record memory or quote
+        //possibly add password feature
                 static void Main(string[] args)
         {
             
-            
+
 
         }
     }
