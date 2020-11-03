ecd9b82ed59154617a367e801a8d8478e12a97df
Jesse Daniels <info@jesseandgena.com>
2020-11-03 14:31:05 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemoriesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
