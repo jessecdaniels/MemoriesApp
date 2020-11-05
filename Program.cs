10574b676c34e17ac7a2880cc6611abea4a3fa07
Jesse Daniels <info@jesseandgena.com>
2020-11-05 13:46:48 -0500

    cleaned up program file by seperating out  menu

--- Program.cs
+++ Program.cs
@@ -3,16 +3,11 @@
 namespace MemoriesApp
 {
     class Program
-    {  
-        //ask childs name 
-        //ask for date
-        //prompt to record memory
-        //possibly add password feature at start?
-                static void Main(string[] args)
+    {
+        static void Main(string[] args)
         {
-            
-
-
+            var MemoriesLog = new MemoriesLogMenu();
+            MemoriesLog.StartMemoriesLogMenu();
         }
     }
 }
